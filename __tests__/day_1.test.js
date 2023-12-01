667df167b09d8bd4ca957c6a0c7c8adf4002b5cc
Alexandre Jung <alexandre.jung@gatewatcher.com>
2023-12-01 14:22:57 +0100

    🎄 day 1

---
+++ __tests__/day_1.test.js
@@ -0,0 +1,47 @@
+const { decode, results } = require('../src/day_1');
+
+describe('day 1', () => {
+  describe('decode', () => {
+    test('15', () => {
+      expect(decode('foo1bar235test')).toBe('15');
+    });
+
+    test('12', () => {
+      expect(decode('foo1bar2')).toBe('12');
+    });
+
+    test('11', () => {
+      expect(decode('foo1')).toBe('11');
+    });
+
+    test('empty', () => {
+      expect(decode('foo')).toBe('');
+    });
+
+    test('3fiveone', () => {
+      expect(decode('3fiveone')).toBe('31');
+    });
+
+    test('qhstsbxsspsrfourmtvtnfhxlj699one', () => {
+      expect(decode('qhstsbxsspsrfourmtvtnfhxlj699one')).toBe('41');
+    });
+
+    test('9ninemdkkqjzjfour9mzspzjgmlhfq', () => {
+      expect(decode('9ninemdkkqjzjfour9mzspzjgmlhfq')).toBe('99');
+    });
+
+    test('dbvjtf294threefournine', () => {
+      expect(decode('dbvjtf294threefournine')).toBe('29');
+    });
+
+    test('eightnineseventwo1seven', () => {
+      expect(decode('eightnineseventwo1seven')).toBe('87');
+    });
+  });
+
+  describe('results', () => {
+    it('should find 54094', () => {
+      expect(results.sumOfCalibrationValues).toBe(54094);
+    });
+  });
+});
---
+++ src/day_1.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const path = require('path');
+
+const ZERO_CHAR_CODE = '0'.charCodeAt(0);
+const NINE_CHAR_CODE = '9'.charCodeAt(0);
+
+const ALPHA_TO_NUMBERS = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+const ALPHA_NUMBERS_REGEX = /^(one|two|three|four|five|six|seven|eight|nine)/;
+
+const currentDir = path.resolve(__dirname);
+const dataPath = `${currentDir}/data/day_1.txt`;
+
+const sumOfCalibrationValues = fs.readFileSync(dataPath)
+  .toString()
+  .split('\n')
+  .map(decode)
+  .map(e => parseInt(e, 10))
+  .filter(value => !isNaN(value))
+  .reduce((acc, v) => acc + v, 0);
+
+function decode(entry) {
+  let first = null;
+  let last = null;
+
+  for (let i = 0; i < entry.length; i++) {
+    const charCode = entry.charCodeAt(i);
+    let number = null;
+
+    if (charCode >= ZERO_CHAR_CODE && charCode <= NINE_CHAR_CODE) {
+      number = entry[i];
+    } else {
+      const substr = entry.substring(i);
+      const search = substr.match(ALPHA_NUMBERS_REGEX);
+
+      if (!search) continue;
+      number = ALPHA_TO_NUMBERS[search[0]];
+      i += search[0].length - 2;
+    }
+
+    if (first === null) first = number;
+    if (number) last = number;
+  }
+
+  return [first, last].join('');
+}
+
+console.log(sumOfCalibrationValues);  // 54094
+
+module.exports = {
+  decode,
+  results: {
+    sumOfCalibrationValues,
+  },
+};
